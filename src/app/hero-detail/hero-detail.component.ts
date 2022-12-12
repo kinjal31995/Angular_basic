199a1d8fe4ea7bef66bddd76ce901d307d8dbcf1
Kinjalben Goalviya <Kinjalben.Goalviya@exxeta.com>
2022-12-12 15:35:16 +0100

    data from server

--- src/app/hero-detail/hero-detail.component.ts
+++ src/app/hero-detail/hero-detail.component.ts
@@ -21,11 +21,16 @@ export class HeroDetailComponent {
   ngOnInit(): void {
     this.getHero();
   }
-  
+
   getHero(): void {
     const id = Number(this.route.snapshot.paramMap.get('id'));
-    this.heroService.getHero(id)
-      .subscribe(hero => this.hero = hero);
+    this.heroService.getHero(id).subscribe((hero) => (this.hero = hero));
+  }
+
+  save(): void {
+    if (this.hero) {
+      this.heroService.updateHero(this.hero).subscribe(() => this.goBack());
+    }
   }
 
   goBack(): void {
--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,7 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HeroService } from '../hero.service';
-import { MessageService } from '../message.service';
 
 @Component({
   selector: 'app-heroes',
@@ -18,8 +17,7 @@ export class HeroesComponent implements OnInit {
   // selectedHero?: Hero;
 
   constructor(
-    private heroService: HeroService,
-    // private messageService: MessageService
+    private heroService: HeroService // private messageService: MessageService
   ) {}
   ngOnInit(): void {
     this.getHeroes();
@@ -36,6 +34,25 @@ export class HeroesComponent implements OnInit {
       .subscribe((heroes: Hero[]) => (this.heroes = heroes));
   } //obeservable
 
+  add(name: string): void {
+    name = name.trim();
+    if (!name) {
+      return;
+    }
+    this.heroService.addHero({ name } as Hero).subscribe((hero) => {
+      this.heroes.push(hero);
+    });
+  }
+
+  delete(hero: Hero): void {
+    this.heroes = this.heroes.filter((h) => h !== hero);
+    this.heroService.deleteHero(hero.id).subscribe();
+  }
+
+  // delete(hero: Hero): void {
+  //   this.heroes = this.heroes.filter((h) => h !== hero);
+  //   this.heroService.deleteHero(hero.id).subscribe();
+  // }
   // getHeroes(): void {
   //   this.heroes = this.heroService.getHeroes();
   // } //normal
